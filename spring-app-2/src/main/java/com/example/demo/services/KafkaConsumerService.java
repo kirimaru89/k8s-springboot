6c86967261f82584e56059fff11f2423c0004693
kirimaru89 <chiennv00377@gmail.com>
2025-03-19 09:49:44 +0700

    add scoped span

--- spring-app-2/src/main/java/com/example/demo/services/KafkaConsumerService.java
+++ spring-app-2/src/main/java/com/example/demo/services/KafkaConsumerService.java
@@ -6,9 +6,9 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.ScopedSpan;
 import com.example.demo.repositories.BookRepository;
 import com.example.demo.models.Book;
-import io.opentelemetry.instrumentation.annotations.WithSpan;
 
 @Service
 public class KafkaConsumerService {
@@ -21,24 +21,26 @@ public class KafkaConsumerService {
     @Autowired
     private Tracer tracer;
     
-    @WithSpan("KafkaConsumerService.listen")
     @KafkaListener(topics = "app-communication", groupId = "${spring.kafka.consumer.group-id}")
     public void listen(String message) {
-        // Log the trace context to verify it's being properly propagated
-        // if (tracer.currentSpan() != null) {
-        //     log.info("Received message with traceId: {}, spanId: {}", 
-        //              tracer.currentSpan().context().traceId(),
-        //              tracer.currentSpan().context().spanId());
-        // }
-        
-        // log.info("Processing message: {}", message);
-
-        var books = bookRepository.findAll();
-        log.info("Processing message:");
-        for (Book book : books) {
-            log.info("Processing message: {}", book.getTitle());
+        // Create and start a scoped span (automatically placed on the current thread)
+        ScopedSpan span = tracer.startScopedSpan("process-kafka-message");
+        try {
+            // Add tags/attributes to the span
+            span.tag("message.type", "kafka");
+            span.tag("kafka.topic", "app-communication");
+            
+            var books = bookRepository.findAll();
+            log.info("Processing message:");
+            for (Book book : books) {
+                log.info("Processing message: {}", book.getTitle());
+            }
+        } catch (Exception e) {
+            span.error(e);
+            throw e;
+        } finally {
+            // Always close the span
+            span.end();
         }
-
-        // throw new RuntimeException("Test exception");
     }
 }

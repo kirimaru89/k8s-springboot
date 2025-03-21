c391444224e91b172c934a899977d07f0b424e1e
kirimaru89 <chiennv00377@gmail.com>
2025-03-21 15:01:16 +0700

    enable redis tracing

--- spring-app-1/src/main/java/com/example/demo/config/RedisTracingConfig.java
+++ spring-app-1/src/main/java/com/example/demo/config/RedisTracingConfig.java
@@ -1,40 +1,40 @@
-// package com.example.demo.config;
+package com.example.demo.config;
 
-// import io.lettuce.core.resource.ClientResources;
-// import io.lettuce.core.tracing.MicrometerTracing;
-// import io.micrometer.observation.ObservationRegistry;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.data.redis.connection.RedisConnectionFactory;
-// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import io.lettuce.core.resource.ClientResources;
+import io.lettuce.core.tracing.MicrometerTracing;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-// @Configuration
-// public class RedisTracingConfig {
+@Configuration
+public class RedisTracingConfig {
 
-//     @Value("${spring.data.redis.host}")
-//     private String redisHost;
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
 
-//     @Value("${spring.data.redis.port}")
-//     private int redisPort;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
 
-//     @Bean
-//     public ClientResources lettuceClientResources(ObservationRegistry observationRegistry) {
-//         MicrometerTracing micrometerTracing = new MicrometerTracing(observationRegistry, "redis");
-//         return ClientResources.builder()
-//                 .tracing(micrometerTracing)
-//                 .build();
-//     }
+    @Bean
+    public ClientResources lettuceClientResources(ObservationRegistry observationRegistry) {
+        MicrometerTracing micrometerTracing = new MicrometerTracing(observationRegistry, "redis");
+        return ClientResources.builder()
+                .tracing(micrometerTracing)
+                .build();
+    }
 
-//     @Bean
-//     public RedisConnectionFactory redisConnectionFactory(ClientResources lettuceClientResources) {
-//         LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
-//                 .clientResources(lettuceClientResources)
-//                 .build();
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(ClientResources lettuceClientResources) {
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientResources(lettuceClientResources)
+                .build();
 
-//         RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
-//         return new LettuceConnectionFactory(redisConfig, clientConfig);
-//     }
-// }
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(redisConfig, clientConfig);
+    }
+}

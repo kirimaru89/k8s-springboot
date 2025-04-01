0db854027bcf548bfac9bdccd661ceb74a596a28
kirimaru89 <chiennv00377@gmail.com>
2025-04-01 16:20:18 +0700

    data source as HikariDataSource

--- spring-app-1/src/main/java/com/example/demo/config/DatabaseConfig.java
+++ spring-app-1/src/main/java/com/example/demo/config/DatabaseConfig.java
@@ -4,17 +4,30 @@ import javax.sql.DataSource;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+    
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    
+    @Value("${spring.datasource.username}")
+    private String username;
+    
+    @Value("${spring.datasource.password}")
+    private String password;
+    
     @Bean
     public DataSource dataSource() {
         HikariDataSource dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl("jdbc:mysql://mysql:3306/book_db?useSSL=false&serverTimezone=UTC");
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUsername("user");
-        dataSource.setPassword("password");
+        dataSource.setJdbcUrl(url);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 }

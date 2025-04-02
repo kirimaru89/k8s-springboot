9d2e11a9f1af9253a4194d356942eb2230535a88
kirimaru89 <chiennv00377@gmail.com>
2025-04-02 09:56:17 +0700

    add call from database app2

--- spring-app-2/src/main/java/com/example/demo/services/AsyncApp1Service.java
+++ spring-app-2/src/main/java/com/example/demo/services/AsyncApp1Service.java
@@ -5,18 +5,28 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.concurrent.CompletableFuture;
+import com.example.demo.repositories.ArtistRepository;
+import com.example.demo.models.Artist;
+import java.util.List;
 
 @Service
 public class AsyncApp1Service {
     private final RestTemplate restTemplate;
+
+    private final ArtistRepository artistRepository;
     
     @Autowired
-    public AsyncApp1Service(RestTemplate restTemplate) {
+    public AsyncApp1Service(RestTemplate restTemplate, ArtistRepository artistRepository) {
         this.restTemplate = restTemplate;
+        this.artistRepository = artistRepository;
     }
 
     @Async
     public CompletableFuture<String> callSpringApp3Async() {
+        List<Artist> artists = artistRepository.findAll();
+        for (Artist artist : artists) {
+            System.out.println("Artist: " + artist.getName());
+        }
         String result = restTemplate.getForObject("http://spring-app-3-service.default.svc.cluster.local:8080/api/test/call-async-flow-to-spring-app-4", String.class);
         return CompletableFuture.completedFuture(result);
     }

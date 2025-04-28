cdbfe87775c94f1cac60407192980faf0aa73a2f
kirimaru89 <chiennv00377@gmail.com>
2025-04-28 09:36:47 +0700

    remove code from producer side (not need from consumer)

--- com.vietinbank.kconsumer/src/main/java/com/vietinbank/kconsumer/config/RabbitMQConfig.java
+++ com.vietinbank.kconsumer/src/main/java/com/vietinbank/kconsumer/config/RabbitMQConfig.java
@@ -12,24 +12,7 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String EXCHANGE_NAME = "my-exchange";
     public static final String QUEUE_NAME = "user-queue";
-    public static final String ROUTING_KEY = "user.created";
-
-    @Bean
-    public DirectExchange exchange() {
-        return new DirectExchange(EXCHANGE_NAME);
-    }
-
-    @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME, true); // durable = true
-    }
-
-    @Bean
-    public Binding binding(Queue queue, DirectExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
-    }
 
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(

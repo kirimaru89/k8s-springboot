8a16afe27a721f02e4f0ff5b2905602139f91b53
kirimaru89 <chiennv00377@gmail.com>
2025-05-08 16:23:05 +0700

    remove unneeded files

--- saga/choreography/fraud-detection-service/src/main/java/com/example/demo/repositories/FraudDetectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repositories;
-
-import com.example.demo.models.FraudDetection;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FraudDetectionRepository extends JpaRepository<FraudDetection, Long> { }

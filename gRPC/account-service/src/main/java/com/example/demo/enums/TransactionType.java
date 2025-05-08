a73ac7e73447540e71fccc404ab4b2561def046b
kirimaru89 <chiennv00377@gmail.com>
2025-05-08 15:13:08 +0700

    cleanup unused code

--- gRPC/account-service/src/main/java/com/example/demo/enums/TransactionType.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.enums;
-
-public enum TransactionType {
-    DEPOSIT,
-    WITHDRAW,
-    TRANSFER
-}
--- gRPC/transaction-service/src/main/java/com/example/demo/enums/TransactionStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.enums;
-
-public enum TransactionStatus {
-    INITIATED,
-    PENDING_RESERVE,
-    COMPLETED,
-    FAILED
-}
